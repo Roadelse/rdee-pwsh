fac345b978b642f9f14a62307c33f8f1b25cb267
rdee.ZJDesktop <roadelse@qq.com>
2024-04-28 16:00:37 +0800

    v24.4.28 | update | add deployment for Windows

--- deploy/deploy.Windows.ps1
+++ deploy/deploy.Windows.ps1
@@ -1,11 +1,12 @@
 #Requires -Version 7
 
 
-$pwshlib = [System.IO.Path]::GetFullPath("$PSScriptRoot\..\powershell")
+$pwshlib = [System.IO.Path]::GetFullPath("$PSScriptRoot\..\src")
 $psmp_current_user = [System.Environment]::GetEnvironmentVariable("PSModulePath", [EnvironmentVariableTarget]::User)
-if (-not $psmp_current_user.Contains($pwshlib)) {
+if ((-not $psmp_current_user) -or (-not $psmp_current_user.Contains($pwshlib))) {
     [Environment]::SetEnvironmentVariable('PSModulePath', "${pwshlib};" + $psmp_current, [EnvironmentVariableTarget]::User)
+    Write-Host "The rdee-pwsh has been installed in system now"
 }
 else {
-    Write-Host "The rdee-pwsh has already been installed in system"
+    Write-Host "Skip! The rdee-pwsh has already been installed in system"
 }
